72570884657f58059e06c941791705871ca553eb
Xavier Fornes <fornes@adobe.com>
2024-12-01 11:35:20 +0100

    Day 01

--- day01/test.py
+++ day01/test.py
@@ -12,29 +12,21 @@ class Test(unittest.TestCase):
         self.input_file = 'input.txt'
 
     def test_p1_example(self):
-        # Test read_pairs function with input file
-        # Add expected pairs for input file if known
         pair_of_lists = read_pair_of_int_lists(self.example_file)
         result = p1(pair_of_lists)
         self.assertEqual(result, 11)
 
     def test_p1_input(self):
-        # Test read_pairs function with input file
-        # Add expected pairs for input file if known
         pair_of_lists = read_pair_of_int_lists(self.input_file)
         result = p1(pair_of_lists)
         self.assertEqual(result, 1879048)
 
     def test_p2_example(self):
-        # Test read_pairs function with input file
-        # Add expected pairs for input file if known
         pair_of_lists = read_pair_of_int_lists(self.example_file)
         result = p2(pair_of_lists)
         self.assertEqual(result, 31)
 
     def test_p2_input(self):
-        # Test read_pairs function with input file
-        # Add expected pairs for input file if known
         pair_of_lists = read_pair_of_int_lists(self.input_file)
         result = p2(pair_of_lists)
         self.assertEqual(result, 21024792)
